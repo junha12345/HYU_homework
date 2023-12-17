18eb1b04ed8519a56a2a4d83050a4e22113aaa7e
arnold6444 <145418649+arnold6444@users.noreply.github.com>
2023-12-17 18:56:32 +0900

    Add files via upload

--- June’s/bank_account.hpp
+++ June’s/bank_account.hpp
@@ -7,35 +7,34 @@ using namespace std;
 
 class BankAccount {
 public:
-  BankAccount(const string& name, unsigned int balance, double interest_rate);
-  virtual ~BankAccount() {};
+    BankAccount(const string& name, string& type, unsigned int balance, double interest_rate) : name_(name),balance_(balance), interest_rate_(interest_rate){};
+    virtual ~BankAccount() {};
 
-  // Compute simple interest rate
-  virtual unsigned int compute_expected_balance(unsigned int n_years_later) const {
-    return balance_ * (1 + interest_rate_ * n_years_later);  
-  }
-  virtual const char* type() const { return "checking"; }
-  const unsigned int& balance() const { return balance_; }
-  const string& name() const { return name_; }
-  const double& interest_rate() const { return interest_rate_; }
+    // Compute simple interest rate
+    virtual unsigned int compute_expected_balance(unsigned int n_years_later) const {
+        return balance_ * (1 + interest_rate_ * n_years_later);
+    }
+    virtual const char* type() const { return "checking"; }
+    const unsigned int& balance() const { return balance_; }
+    const string& name() const { return name_; }
+    const double& interest_rate() const { return interest_rate_; }
 
 protected:
-  string name_;
-  unsigned int balance_;
-  double interest_rate_;
+    string name_;
+    unsigned int balance_;
+    double interest_rate_;
 };
 
 class SavingsAccount : public BankAccount {
 public:
-  SavingsAccount(const string& name, int balance, double interest_rate);
-  virtual ~SavingsAccount() {};
+    SavingsAccount(const string& name, string& type, int balance, double interest_rate):BankAccount(name,type,balance,interest_rate) {};
+    virtual ~SavingsAccount() {};
 
-  virtual const char* type() const { return "savings"; }
-  // Compute compound interest rate
-  virtual unsigned int compute_expected_balance(
-    unsigned int n_years_later) const {
-    return balance_ * pow(1 + interest_rate_, n_years_later);
-  }
+    virtual const char* type() const { return "savings"; }
+    // Compute compound interest rate
+    virtual unsigned int compute_expected_balance(unsigned int n_years_later) const {
+        return balance_ * pow(1 + interest_rate_, n_years_later);
+    }
 };
 
 #endif
--- June’s/bank_account_main.cpp
+++ June’s/bank_account_main.cpp
@@ -5,43 +5,71 @@
 #include <string>
 #include <vector>
 #include "bank_account.hpp"
-#include "bank_account.cpp"
 using namespace std;
 
 int main(void) {
-  string command;
-  vector<BankAccount*> accounts;
-  vector<BankAccount*>::iterator it;
-  bool quitEntered = false;
-  while (!quitEntered && getline(cin, command)) {
-    BankAccount* a;
-    string name, type, file_name;
-    unsigned int balance;
-    double interest_rate;
-    vector<BankAccount*>::iterator last;
-    int n_years_later; 
-    char op = command[0];
-    istringstream iss(command.substr(1));
-    switch (op) {
-      case 'C':
-		// TODO
-        break;
-      case 'D':
-		// TODO
-        break;
-      case 'P':
-		// TODO
-        break;
-      case 'A':
-		// TODO
-        break;
-      case 'Q':
-		// TODO
-        break;
-      default:
-        cerr<<"Undefined command"<<endl;
-        exit(1);
+    string command;
+    vector<BankAccount*> accounts;
+    vector<BankAccount*>::iterator it;
+    bool quitEntered = false;
+    while (!quitEntered && getline(cin, command)) {
+        BankAccount* a;
+        string name, type, file_name;
+        unsigned int balance;
+        double interest_rate;
+        vector<BankAccount*>::iterator last;
+        int n_years_later;
+        char op = command[0];
+        istringstream iss(command.substr(1));
+        switch (op) {
+        case 'C':
+            // TODO
+            iss >> name >> type >> balance >> interest_rate;
+            if (type == "checking") {
+                a = new BankAccount(name, type, balance, interest_rate);
+            }
+            else if (type == "savings") {
+                a = new SavingsAccount(name, type, balance, interest_rate);
+            }
+            accounts.push_back(a);
+            break;
+        case 'D':
+            // TODO
+            iss >> name;
+            for (BankAccount* acc : accounts) {
+                if (acc->name() == name) {
+                        delete acc;
+                    }
+            }
+            break;
+        case 'P':
+            // TODO
+            for (BankAccount* acc : accounts) {
+                cout << acc->name() << "      " << acc->type() << "      " << acc->balance() << "      " << acc->interest_rate() << endl;
+            }
+            break;
+        case 'A':
+            // TODO
+            iss >> n_years_later;
+            for (BankAccount* acc : accounts) {
+                unsigned int expected_balance = acc->compute_expected_balance(n_years_later);
+                cout << acc->name() << "      " << acc->type()
+                    << "      " << expected_balance << "      " << acc->interest_rate() << endl;
+            }
+            break;
+        case 'Q':
+            // TODO
+            quitEntered = true;
+            break;
+        default:
+            cerr << "Undefined command" << endl;
+            exit(1);
+        }
     }
-  }
+    for (BankAccount* acc : accounts) {
+        delete acc;
+    }
+    accounts.clear();
 }
+
 
